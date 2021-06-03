2264013558723d2f632f21a5727e5dd02dfc6327
dmytro.hrynevych <dmytro.hrynevych@hermes-europe.co.uk>
2021-06-03 21:14:42 +0300

    init

---
+++ avito_auto/src/test/java/SampleTest.java
@@ -0,0 +1,36 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class SampleTest {
+    private static final String BASE_URL = "https://www.avito.ru";
+    private static final String EMAIL = "tatadonetskaya@gmail.com";
+    private static final String PASSWORD = "xN6T!VxqfLjN6ay";
+
+
+    @Test
+    public void logoutTest() {
+        System.out.println("hello");
+    }
+
+    @Test
+    public void loginAvitoTest() {
+        Configuration.browser = "chrome";
+        Configuration.baseUrl = BASE_URL;
+        Configuration.pageLoadTimeout = 150000;
+        Selenide.open("/profile/settings");
+
+        $("[name='login']").sendKeys(EMAIL);
+        $("[name='password']").sendKeys(PASSWORD);
+        $("[name='submit']").click();
+        Selenide.open("/sochi/lichnye_veschi?cd=1&d=1");
+        $$("[data-marker='catalog-serp']  [itemprop='name']").get(5).click();
+        $(byText("Купить с доставкой")).click();
+        $("[name='phone']").shouldHave(Condition.value(""));
+    }
+}
